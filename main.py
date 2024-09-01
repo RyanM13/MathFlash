8f63a4886b7d3aebfa5dbfe6462f34e60b7a3373
Ryan <mathewsryan2@gmail.com>
2024-09-01 00:18:07 -0500

    finished calc section, created trig section added theta sign, added point system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PyQt5.QtWidgets import QWidget, QGridLayout, QPushButton, QLabel, QApplication, QLineEdit
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QPixmap
 import random
 
 import sys
@@ -13,6 +13,10 @@ class MainWindow(QWidget):
 
     def initui(self):
         self.setWindowTitle("Calc Memorization")
+        
+        self.miss_counter = 0
+        self.point_system = 0
+        self.answers_correct = 0
 
         self.setWindowIcon(QIcon("mathicon1_s97_icon.ico"))
         self.layout = QGridLayout() 
@@ -51,6 +55,13 @@ class MainWindow(QWidget):
 
     def SetTrig(self):
         self.Title_Screen(self.Trig)
+        self.lastchoice = None
+
+    def SetCalcCheck(self):
+        self.check_answer(self.calc_answers)
+
+    def SetTrigCheck(self):
+        self.check_answer(self.trig_answers)
     
     def Calc(self):
         self.clear_layout()
@@ -74,6 +85,8 @@ class MainWindow(QWidget):
             "arc sec x": "1/x√x^2 - 1",
             "arc csc x": "-1/x√x^2 - 1" 
     }
+
+        
 
         self.calc_functions = list(self.calc_answers.keys())
 
@@ -112,14 +125,86 @@ class MainWindow(QWidget):
         # Submit button
         self.submit_button = QPushButton("Submit", self)
         self.submit_button.setStyleSheet("font-size: 16px; font-weight: bold; background-color: #4CAF50; color: white; padding: 5px 10px;")
-        self.submit_button.clicked.connect(self.check_answer)
+        self.submit_button.clicked.connect(self.SetCalcCheck)
         self.user_answer.returnPressed.connect(self.submit_button.click)
         self.layout.addWidget(self.submit_button, 2, 0, 1, 4, Qt.AlignCenter)
 
         
     
     def Trig(self):
-        print("Placeholder")
+        self.clear_layout()
+        self.trig_answers = {
+            "tan θ": "sin θ/cos θ",
+            "cot θ": "cos θ/sin θ",
+            "cos[π/2 - θ]": "sin θ",
+            "sin[π/2 - θ]": "cos θ",
+            "cot[π/2 - θ]": "tan θ",
+            "tan[π/2 - θ]": "cot θ",
+            "sec[π/2 - θ]": "cosec θ",
+            "cosec[π/2 - θ]": "sec θ",
+            "cot θ": "1/tan θ",
+            "cosec θ": "1/sin θ",
+            "sec θ": "1/cos θ",
+            "sin(-θ)": "-sin θ",
+            "cos(-θ)": "cos θ",
+            "tan(-θ)": "-tan θ",
+            "cot(-θ)": "-cot θ",
+            "sec(-θ)": "sec θ",
+            "cosec(-θ)": "-cosec θ",
+            "sin^2θ + cos^2θ": "1",
+            "1 + tan^2θ": "sec^2θ", 
+            "1 + cot^θ": "cosec^2θ", 
+             
+            
+        }
+
+
+        self.trig_functions = list(self.trig_answers.keys())
+
+        self.random_choice = random.choice(self.trig_functions)
+        while self.random_choice == self.lastchoice:
+            self.random_choice = random.choice(self.trig_functions)   
+            
+            
+        self.lastchoice = self.random_choice
+        
+        self.Display_label = QLabel(self.random_choice , self)
+        self.Display_label.setStyleSheet("font-size: 32px; font-weight: bold; color: #333;")
+        self.layout.addWidget(self.Display_label, 0, 0, 1, 4, Qt.AlignCenter)
+
+        # Label for the answer
+        self.answer_label = QLabel("Answer:", self)
+        self.answer_label.setStyleSheet("font-size: 18px; font-weight: bold;")
+        self.layout.addWidget(self.answer_label, 1, 0, Qt.AlignRight)
+
+        # User answer input
+        self.user_answer = QLineEdit(self)
+        self.user_answer.setFixedSize(100, 30)  # Increased size for easier input
+        self.user_answer.setStyleSheet("font-size: 16px; padding: 5px;")
+        self.layout.addWidget(self.user_answer, 1, 1, Qt.AlignLeft)
+
+        # Radical button
+        self.radical_button = QPushButton("√", self)
+        self.radical_button.setFixedSize(50, 30)
+        self.radical_button.clicked.connect(self.radicalsign)
+        self.layout.addWidget(self.radical_button, 1, 2, Qt.AlignLeft)
+
+        self.theta_button = QPushButton("θ", self)
+        self.theta_button.setFixedSize(50,30)
+        self.theta_button.clicked.connect(self.thetasign)
+        self.layout.addWidget(self.theta_button, 1,3, Qt.AlignLeft)
+
+        # Empty stretch to center the elements horizontally
+        self.layout.setColumnStretch(0, 1)
+        self.layout.setColumnStretch(3, 1)
+
+        # Submit button
+        self.submit_button = QPushButton("Submit", self)
+        self.submit_button.setStyleSheet("font-size: 16px; font-weight: bold; background-color: #4CAF50; color: white; padding: 5px 10px;")
+        self.submit_button.clicked.connect(self.SetTrigCheck)
+        self.user_answer.returnPressed.connect(self.submit_button.click)
+        self.layout.addWidget(self.submit_button, 2, 0, 1, 4, Qt.AlignCenter)
+
 
          
 
@@ -156,6 +241,10 @@ class MainWindow(QWidget):
         self.user_answer.setText(current_text + "√")
 
 
+    def thetasign(self):
+        current_text = self.user_answer.text()
+        self.user_answer.setText(current_text + "θ")
+
 
 
         
@@ -166,17 +255,20 @@ class MainWindow(QWidget):
         for i in reversed(range(self.layout.count())): 
          self.layout.itemAt(i).widget().setParent(None)
 
-    def check_answer(self):
+    def check_answer(self,answers):
         self.clear_layout()
         user_input = self.user_answer.text().strip().lower()
-        self.correct_answer = self.calc_answers[self.random_choice].strip().lower()
-        miss_counter = 0
+        self.correct_answer = answers[self.random_choice].strip().lower()
         
         if user_input == self.correct_answer:
-            #self.answers_correct = self.answers_correct + 1
-           # if self.answers_correct == 5:
-                #self.point_system *= 2
-            #self.point_system = self.point_system + 100
+            self.answers_correct += 1
+            if self.answers_correct > 5:
+                self.point_system *= 2
+            else:
+                self.point_system += 100
+
+            self.Point_label = QLabel(f"Points: {self.point_system}")
+            self.layout.addWidget(self.Point_label, 0,3, Qt.AlignRight)
             self.correct_display = QLabel("Correct!")
             self.correct_display.setStyleSheet("font-weight: bold; font-size: 48px; color: green")
             self.layout.addWidget(self.correct_display, 0,1, Qt.AlignCenter)
@@ -185,6 +277,8 @@ class MainWindow(QWidget):
             self.wrong_display = QLabel("Incorrect")
             self.wrong_display.setStyleSheet("font-weight: bold; font-size: 48px; color: red")
             self.layout.addWidget(self.wrong_display, 0, 1, Qt.AlignCenter)
+
+            self.answers_correct = 0
             
           
 
@@ -194,9 +288,9 @@ class MainWindow(QWidget):
             
             QTimer.singleShot(3500, self.Calc)
 
-            miss_counter +=1
+            self.miss_counter +=1
 
-            if miss_counter == "3": 
+            if self.miss_counter == "3": 
                 self.retry_button = QPushButton("Retry?")
                 self.retry_button.clicked.connect(self.Calc)
                 self.layout.addWidget(self.retry_button, 0, 1, Qt.AlignCenter | Qt.AlignBottom)
